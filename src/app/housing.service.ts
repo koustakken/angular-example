b89bda35a3201d0d9167c456099b7823cbf38027
Artem Ivanov <ivanov@hostco.ru>
2023-12-04 13:16:24 +0500

    forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -123,5 +123,7 @@ export class HousingService {
 		);
 	}
 
-
+	submitApplication(firstName: string, lastName: string, email: string) {
+		console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`);
+	}
 }
